37099539159b89daf711cc2b0d28e0d39dc18b98
littlecorny <carlapajuelopaniagua2.0@gmail.com>
2025-10-01 14:02:16 +0200

    Create funciones.js

---
+++ java/funciones.js
@@ -0,0 +1,108 @@
+/*function holahola() {
+    console.log("Hola");
+}
+
+holahola ();
+
+function hola (nombre, edad) {
+    console.log ("Hola" + nombre)
+    console.log ("Tienes" + edad + "años")
+}
+
+hola(" Inma", " 23 ")
+
+function adeu (nombre, apellido) {
+    console.log ("Hola" + nombre + apellido)
+}
+
+adeu (" Ana ", "Hernández ")
+
+function suma (a, b) {
+    console.log ("Suma" + a + b)
+}
+
+suma (5,6)
+
+function hola (nombre="amigo"){
+    console.log("Hola "+ nombre)
+}
+
+hola ()
+hola ("Ana")
+
+
+function suma (a=6, b=3) {
+    console.log (a+b)
+}
+
+suma (1, 2);
+
+function saludarPersona (nombre="Loli") {
+    console.log ("Holi " + nombre)}
+
+saludarPersona ();
+
+//
+
+function nombreFuncion(a=2, b=5) {
+    const datos = (a * b) /3;
+    return resultado;
+}
+
+const miResultado = nombreFuncion(datos);
+
+function resta (a,b) {
+    return a-b;
+}
+let resultado=resta(4,2);
+console.log(resultado);
+
+//
+
+// calculadora amono
+
+function calculadora (a,b,operacion="sumar") {
+    let resultado;
+    
+    if (operacion == "sumar") {
+        resultado=a+b;
+    } else if (operacion == "restar") {
+        resultado=a-b;
+    } else if (operacion == "multiplicar") {
+        resultado=a*b;
+    } else if (operacion == "dividir") {
+        resultado=a/b;
+    }
+
+    console.log("El resultado es " + resultado); //console.log siempre va entre estos dos corchetes
+}   
+
+//llamar a la funcion desde fuera
+calculadora (8,2,"restar");
+calculadora (8,2,"sumar");
+calculadora (8,2,"multiplicar");
+calculadora (8,2,"dividir");
+
+
+
+function sumar (a,b) {
+    let resultado;
+    console.log ("El resultado es " + (a+b))
+}
+
+sumar(6,2)
+
+
+
+
+const saludar = (nombre) => {
+    return "¡Hola, " + nombre + "!";
+}
+
+console.log(saludar("Ana"))
+
+
+const sumar = (a, b) => a + b;
+console.log(sumar(6,3))
+
+*/
